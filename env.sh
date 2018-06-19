7745da7582121366cfd7c0409d64a0a396235188
Allen Day <allenday@allenday.com>
2018-06-19 21:15:22 +0700

    project environment vars

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export NAME="bwa-rice"
+export REGION="us-east1"
+export ZONE="${REGION}-d"
+export MACHINE_TYPE="n1-standard-1"
+export MIN_REPLICAS=1
+export MAX_REPLICAS=3
+export TARGET_CPU_UTILIZATION=0.8
