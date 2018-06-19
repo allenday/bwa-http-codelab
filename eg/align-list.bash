3d9e7ad34a74adf09e16a18514c0592c4b675b09
Allen Day <allenday@allenday.com>
2018-06-19 23:09:11 +0700

    alignment examples from rice3k

---
+++ eg/align-list.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for sample in `cat todo2.txt | sort -r`; do
+	echo $sample;
+	gsutil -m cp -r gs://rice-3k/PRJEB6180/primary/$sample . ;
+	rm -f $sample.fq ;
+	for i in $sample/*.bam; do
+		samtools bam2fq $i >> $sample.fq ;
+		rm -f $i;
+	done ;
+	rm -rf $sample ;
+	cat $sample.fq | bwa mem /align/all.con - -p -t 32 > $sample.sam ;
+	rm -f $sample.fq ;
+	samtools view -S -@ 32 -b $sample.sam > $sample.pre ;
+	rm -f $sample.sam ;
+	samtools sort -@ 32 $sample.pre > $sample.bam ;
+	rm -f $sample.pre ;
+	samtools index $sample.bam $sample.bam.bai ;
+	gsutil -m cp $sample.bam gs://rice-3k/PRJEB6180/aligned-Os-Nipponbare-Reference-IRGSP-1.0/ ;
+	gsutil -m cp $sample.bam.bai gs://rice-3k/PRJEB6180/aligned-Os-Nipponbare-Reference-IRGSP-1.0/ ;
+	rm -rf $sample.* ;
+	rm -f /align/*.bam ;
+done
